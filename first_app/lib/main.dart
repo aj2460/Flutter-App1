3ae694ff65291877d52b4b4ed324afaf9fb0cc81
Anand <ajkadakkal@gmail.com>
2021-11-10 19:24:05 +0530

    Initial Commit

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello"),
+      home: Text("Hello World"),
     );
   }
 }
