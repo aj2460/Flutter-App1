f3e8ac68a884b11097557d2b19bc39874af248a5
Anand <ajkadakkal@gmail.com>
2021-11-10 21:23:07 +0530

    Add State to widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,17 +4,56 @@ import 'package:flutter/material.dart';
 //   runApp(myApp());
 // }
 
-void main() => runApp(myApp());
+void main() => runApp(MyApp());
 
-class myApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerClickd() {
+    setState(() {
+      questionIndex++;
+    });
+
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite anima?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My First App"),
         ),
-        body: const Text("This is my default Text"),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: answerClickd,
+              child: const Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerClickd,
+              child: const Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerClickd,
+              child: const Text("Answer 3"),
+            ),
+          ],
+        ),
       ),
     );
   }
